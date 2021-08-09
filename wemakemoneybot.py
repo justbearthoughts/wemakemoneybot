223d2226309f4802fa38002bf1eda7e400716a00
justbearthoughts <justbearthoughts@protonmail.com>
2021-08-09 13:58:42 +0200

    Catch TimeoutError
    Signed-off-by: justbearthoughts <justbearthoughts@protonmail.com>

--- wemakemoneybot.py
+++ wemakemoneybot.py
@@ -295,5 +295,5 @@ if __name__ == '__main__':
         try:            
             loop = asyncio.get_event_loop()
             loop.run_until_complete(main())
-        except (prawcore.exceptions.ServerError, prawcore.exceptions.ResponseException):
+        except (prawcore.exceptions.ServerError, prawcore.exceptions.ResponseException, asyncio.exceptions.TimeoutError):
             continue
